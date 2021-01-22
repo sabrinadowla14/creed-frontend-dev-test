faf5f3aea015e78bbec151270c40021d1bbacf9d
Sabrina Dowla <yourGitHub@email.com>
2021-01-21 18:38:52 -0600

    Updated Files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [{ path: 'podcasts', loadChildren: () => import('./podcasts/podcasts.module').then(m => m.PodcastsModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'podcasts'
+  },
+  {
+    path: 'podcasts',
+    loadChildren: () =>
+      import('./podcasts/podcasts.module').then(m => m.PodcastsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
